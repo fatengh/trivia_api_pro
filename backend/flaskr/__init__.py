9efbff25524958e3988db64acabaac0a76710f6e
fatengh <faten.arg@hotmail.com>
2020-08-20 23:48:15 +0300

    new update

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -33,8 +33,7 @@ def create_app(test_config=None):
   # create and configure the app
  app = Flask(__name__)
  setup_db(app)
- migrate = Migrate(app)
- db_drop_and_create_all()
+ 
 
 
 
--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -9,16 +9,15 @@ from flaskr import create_app
 class TriviaTestCase(unittest.TestCase):
     """This class represents the trivia test case"""
 
-   def setUp(self):
+    def setUp(self):
         """Define test variables and initialize app."""
-      self.app = create_app()
-      self.client = self.app.test_client
-      self.database_name = "trivia_test"
-      self.database_path = "postgres://{}/{}".format(
-            'localhost:5432', self.database_name)
-      setup_db(self.app, self.database_path)
+        self.app = create_app()
+        self.client = self.app.test_client
+        self.database_name = "trivia_test"
+        self.database_path = "postgres://{}/{}".format( 'localhost:5432', self.database_name)
+        setup_db(self.app, self.database_path)
         
-      with self.app.app_context():
+        with self.app.app_context():
            self.db = SQLAlchemy()
            self.db.init_app(self.app)
             # create all tables
